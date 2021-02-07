64c391b7b4797a5281f5f960114ab0878b40c08f
ju-to <088t3748@gmail.com>
2021-02-08 07:04:30 +0900

    workモデルの作成及びcrud処理を実装

--- Gemfile
+++ Gemfile
@@ -53,3 +53,4 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem "simple_calendar", "~> 2.4"
+gem 'rails-i18n'
---
+++ app/controllers/works_controller.rb
@@ -0,0 +1,49 @@
+class WorksController < ApplicationController
+
+  before_action :set_work, only: %i[show edit update destroy]
+
+  def index
+    @works = Work.all
+  end
+
+  def show
+
+  end
+
+  def new
+    @work = Work.new
+  end
+
+  def create
+    Work.create(work_params)
+    redirect_to root_path, notice: "登録しました"
+  end
+
+  def edit
+
+  end
+
+  def update
+    if @work.update!(work_params)
+      redirect_to @work, notice: "編集しました"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @work.destroy
+    redirect_to root_path, notice: "削除しました"
+  end
+
+
+  private
+
+  def set_work
+    @work = Work.find(params[:id])
+  end
+
+  def work_params
+    params.require(:work).permit(:name, :content, :user_name, :required_skill, :start_time, :end_time)
+  end
+end
